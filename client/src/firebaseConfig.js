16f77bd2cb066071d7b72e0c437b133897df7c60
Mike Corbett <srcorbetto@gmail.com>
2019-03-28 21:27:40 -0500

    Setting up the game

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyC7q2Z_iuEHFUiH10zmizAhzoMXP0FE65M",
+    authDomain: "bash-bash.firebaseapp.com",
+    databaseURL: "https://bash-bash.firebaseio.com",
+    projectId: "bash-bash",
+    storageBucket: "",
+    messagingSenderId: "446079285310"
+  };
